3727213962798629a3599478cb9e73ad7772e89c
Ryan Crosser <rmc9yd@virginia.edu>
2020-09-30 12:46:18 -0400

    test2

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE HTML>
+<?php ?>
+<!--
+	Industrious by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Welcome - Industrious by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<a class="logo" href="index.html">NotAmazon.com</a>
+				<nav>
+					<a href="#menu">Menu</a>
+				</nav>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="elements.html">Elements</a></li>
+					<li><a href="generic.html">Generic</a></li>
+				</ul>
+			</nav>
+
+		<!-- Heading -->
+			<div id="heading" >
+				<h1>Welcome</h1>
+			</div>
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="inner">
+					<div class="content">
+						<header>
+							<h2>Your Task</h2>
+						</header>
+						<p>Today your task is to buy a new watch battery from NotAmazon.com.</p>
+						<hr />
+						<h3>Information</h3>
+						<p>Part Number: 8914238432123198703</p>
+						<p>Credit Card Number:4567829443871038</p>
+					</div>
+				</div>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+
+<footer id="footer">
+				<div class="inner">
+					<div class="content">
+						<section>
+							<li><a href="index.html">Next</a></li>
+						</section>
+						<section>
+							<li><a href="index.html">Next</a></li>
+						</section>
+						<section>
+							<li><a href="index.html">Next</a></li>
+						</section>
+						<section>
+							<li><a href="index.html">Next</a></li>
+						</section>
+
+					</div>
+					<div class="copyright">
+						&copy; Untitled. Photos <a href="https://unsplash.co">Unsplash</a>, Video <a href="https://coverr.co">Coverr</a>.
+					</div>
+				</div>
+			</footer>
+
+
+</html>
